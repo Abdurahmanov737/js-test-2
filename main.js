4cf61630ecf9a363b406a733c2629f1aa57a3801
Abdurahmanov737 <anaraof@gmail.com>
2021-08-31 13:53:43 +0400

    08-31-20221

---
+++ main.js
@@ -0,0 +1,62 @@
+// 7 ci sual
+
+var arr = [10, 68, 987, 7, 1, 8]
+
+var max = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+        max = arr[i]
+    }
+}
+console.log(max);
+
+// --------------------------------------------------//
+
+
+
+
+// 8 ci sual
+
+var km = 15;
+var m = km * 1000;
+console.log(m);
+
+
+// --------------------------------------------------//
+
+
+
+
+// 15 ci sual
+
+function myLength(a) {
+
+    var ownLength = 0;
+
+    while (a[ownLength] != undefined){
+      ownLength++;
+    } 
+    return ownLength;
+  }
+  
+  console.log(myLength("Salamlar")); 
+  
+
+// --------------------------------------------------//
+
+
+
+
+// 16 ci sual
+
+
+
+var a = Math.floor(Math.random()*101)
+
+console.log(a);
+
+
+// 24cu cehdde 81 print oldu
+
+// --------------------------------------------------//
